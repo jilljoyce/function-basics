99fcea703b6f5e6dd64fd660fcd91d0f9cf6e0cc
jilljoyce <46486508+jilljoyce@users.noreply.github.com>
2021-04-14 10:39:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function pizzaMakingSteps() {
+    console.log("Pak deeg en rol dat uit tot een pizza");
+    console.log("Doe er tomatensaus op");
+    console.log("Beleg de pizza met wat jij wil!");
+    console.log("Doe de pizza 30 minuten in de oven");
+}
+
+pizzaMakingSteps();
+
+/* zo kan het ook (winc manier): 
+
+const pizzaMakingStepsTWO = function() {
+    console.log("Pak deeg en rol dat uit tot een pizza");
+    console.log("Doe er tomatensaus op");
+    console.log("Beleg de pizza met wat jij wil!");
+    console.log("Doe de pizza 30 minuten in de oven");
+}
+
+pizzaMakingStepsTWO();
+*/
+
+function sushiMakingSteps() {
+    console.log("Kook rijst");
+    console.log("Beleg het met verse vis");
+    console.log("Rol het op met zeewier");
+}
+
+sushiMakingSteps();
+sushiMakingSteps();
+sushiMakingSteps();
+
+pizzaMakingSteps();
